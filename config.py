10dad071989ac8dc7ea691c8f509a1ae12e5d68c
Vladimir <v.s.shkodin@gmail.com>
2019-09-20 11:17:29 -0700

    added config update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 
-apppath= "C:\\Users\\Vladimir\\Desktop\\app-debug.apk"
+apppath= "\Vladimir\\app-debug.apk"
 platformName='Android'
 
 ################# FIRST DEVICE ###############################
@@ -9,7 +9,7 @@ desiredCapabilitiesAppiumFirstddevice={ "deviceName": "KYV7N15B14001804",
                                         "platformName": platformName,
                                         "app": apppath}
 
-urlFirstAppiumDockerServer='http://127.0.0.1:4723/wd/hub'#http://172.28.49.81:4723/wd/hub'
+urlFirstAppiumDockerServer='http://172.28.49.81:4723/wd/hub'#http://172.28.49.81:4723/wd/hub'
 
 
 ################# SECOND DEVICE ###############################
@@ -20,4 +20,4 @@ desiredCapabilitiesAppiumSeconddevice={ "deviceName": "Z33201905000095",
                                         "app": apppath}
 
 
-urlSecondAppiumDockerServer='http://127.0.0.1:4724/wd/hub'#http://172.28.49.81:4724/wd/hub'
+urlSecondAppiumDockerServer='http://172.28.49.81:4724/wd/hub'#http://172.28.49.81:4724/wd/hub'
